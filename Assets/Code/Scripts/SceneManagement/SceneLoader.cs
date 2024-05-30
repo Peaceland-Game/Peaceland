f836ba41fc6c31c757271cda4364902649b6a933
Michael Eaton (RIT Student) <me3870@rit.edu>
2024-05-30 15:41:39 -0400

    scene loading

--- Assets/Code/Scripts/SceneManagement/SceneLoader.cs
+++ Assets/Code/Scripts/SceneManagement/SceneLoader.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // For UI elements (loading screen)
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<string> sceneNames = new List<string>(); // List of scenes to load
+    public GameObject loadingScreen; // Reference to the loading screen GameObject
+    public Slider progressBar; // Reference to the loading progress bar (optional)
+
     void Start()
     {
-        
+        StartCoroutine(LoadScenesOneByOne());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadScenesOneByOne()
     {
-        
+        // Show loading screen
+        if (loadingScreen != null)
+        {
+            loadingScreen.SetActive(true);
+        }
+
+        foreach (string sceneName in sceneNames)
+        {
+            yield return StartCoroutine(LoadAdditiveScene(sceneName));
+        }
+
+        // Hide loading screen
+        if (loadingScreen != null)
+        {
+            loadingScreen.SetActive(false);
+        }
+    }
+
+    IEnumerator LoadAdditiveScene(string sceneName)
+    {
+        // Check if the scene is already loaded
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            // Start loading the scene asynchronously
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+            // While the scene is loading, update the progress bar (if available)
+            while (!asyncLoad.isDone)
+            {
+                if (progressBar != null)
+                {
+                    progressBar.value = asyncLoad.progress;
+                }
+                yield return null;
+            }
+
+            Debug.Log($"Scene {sceneName} loaded additively.");
+        }
+        else
+        {
+            Debug.Log($"Scene {sceneName} is already loaded.");
+        }
     }
 }
