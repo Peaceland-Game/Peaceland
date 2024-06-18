682214ccfa893881508c681c1bb7837d8fba0acd
CodingSceptile <70319436+CodingSceptile@users.noreply.github.com>
2024-06-18 14:11:26 -0400

    coded so that name moves left when the subtitle panel gets longer

--- Assets/Code/Scripts/UI/DialogueUILayoutAdjuster.cs
+++ Assets/Code/Scripts/UI/DialogueUILayoutAdjuster.cs
@@ -4,9 +4,14 @@ using PixelCrushers.DialogueSystem;
 public class DialogueUILayoutAdjuster : MonoBehaviour
 {
     public RectTransform subtitlePanel;
+    public RectTransform subtitlePanelText;
+    public RectTransform speakerName;
   //  public RectTransform menuPanel;
     public Vector2 subtitlePanelExpandedSize;
     public Vector2 subtitlePanelDefaultSize;
+
+    public Vector2 subtitlePanelTextExpandedSize;
+    public Vector2 subtitlePanelTextDefaultSize;
 
     private DialogueSystemEvents dialogueSystemEvents;
 
@@ -25,13 +30,20 @@ public class DialogueUILayoutAdjuster : MonoBehaviour
             // No responses, expand subtitle panel
             Debug.Log("Expand subtitle panel");
             subtitlePanel.sizeDelta = subtitlePanelExpandedSize;
-           // menuPanel.gameObject.SetActive(false);
+            subtitlePanelText.sizeDelta = subtitlePanelTextExpandedSize;
+            
+            //hard coding the values atm should probably figure out a way to not
+            speakerName.anchoredPosition = new Vector2(653, -18);
+            // menuPanel.gameObject.SetActive(false);
         }
         else
         {
             // Responses available, revert to default size
             Debug.Log("revert subtitle panel");
             subtitlePanel.sizeDelta = subtitlePanelDefaultSize;
+            subtitlePanelText.sizeDelta = subtitlePanelTextDefaultSize;
+
+            speakerName.anchoredPosition = new Vector2(353, -18);
           //  menuPanel.gameObject.SetActive(true);
         }
     }
@@ -40,5 +52,6 @@ public class DialogueUILayoutAdjuster : MonoBehaviour
     {
         // Revert to default size when conversation ends
         subtitlePanel.sizeDelta = subtitlePanelDefaultSize;
+        speakerName.anchoredPosition = new Vector2(353, -18);
     }
 }
