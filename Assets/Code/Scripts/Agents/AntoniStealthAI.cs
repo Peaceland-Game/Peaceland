1380b20078ba638defedb16c3baa65a65804e012
Michael Eaton <lammara96@gmail.com>
2024-07-01 13:31:15 -0400

    road shader update, antoni stealth update

--- Assets/Code/Scripts/Agents/AntoniStealthAI.cs
+++ Assets/Code/Scripts/Agents/AntoniStealthAI.cs
@@ -31,10 +31,13 @@ public class AntoniStealthAI : MonoBehaviour
     private float seekTime;
     private float currentSeekTime;
 
+    private Stealth stealthScript;
+
     void Start()
     {
         currentState = State.Patrol;
-        currentSeekTime = gameObject.GetComponent<Stealth>().secondsAgentSeeks;
+        stealthScript = GetComponent<Stealth>();
+        currentSeekTime = stealthScript.secondsAgentSeeks;
         seekTime = currentSeekTime;
         currentDirection = 1;
         directionTimer = 0;
@@ -57,97 +60,108 @@ public class AntoniStealthAI : MonoBehaviour
 
         agent.destination = playerPoint.position;
     }
+    void Patrol()
+    {
+        agent.isStopped = false;
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GoToNextPoint();
 
+        //if they spot the player, seek them
+        if (stealthScript.detectedPlayer)
+        {
+            currentSeekTime = 0;
+            currentState = State.Seek;
+        }
+
+        //if they hear the player, go on alert
+        if (stealthScript.heardPlayer)
+        {
+            currentState = State.Alert;
+        }
+    }
+    void Alert()
+    {
+        //stop pathfinding
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+
+        //rotate around y axis
+        transform.Rotate(0, rotationPerSecond * currentDirection * Time.deltaTime, 0, 0);
+
+        //every 1/10th of a second, check if the agent should change direction
+        directionTimer += Time.deltaTime;
+        if (directionTimer >= 0.1f && (int)(Random.value * 100) <= percentChanceToChangeRotation)
+        {
+            currentDirection *= -1;
+        }
+
+        if (directionTimer >= 0.1f)
+        {
+            directionTimer = 0;
+        }
+
+        //after a bit of no sound, go back on patrol
+        if (!stealthScript.heardPlayer)
+        {
+            currentState = State.Patrol;
+            currentWaypoint--;
+            GoToNextPoint();
+        }
+
+        //if they see the player, seek them
+        if (stealthScript.detectedPlayer)
+        {
+            currentSeekTime = 0;
+            currentState = State.Seek;
+        }
+    }
+    void Seek()
+    {
+        agent.isStopped = false;
+        agent.velocity = Vector3.zero;
+        GoToPlayerPoint();
+
+        //seek time increases
+        currentSeekTime += Time.deltaTime;
+
+        //if they see the player again or are within the seek radius, reset the time
+        if (stealthScript.detectedPlayer ||
+            stealthScript.distanceToPlayer <= stealthScript.alwaysPersueRadius)
+        {
+            currentSeekTime = 0;
+        }
+
+        //if the agent catches the player, FOR NOW, go back to patrol
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            currentState = State.Patrol;
+            GoToNextPoint();
+        }
+
+        //if the agent loses the player after a bit, go on alert
+        if (!stealthScript.detectedPlayer && currentSeekTime >= seekTime)
+        {
+            currentState = State.Alert;
+        }
+    }
     void Update()
     {
-        Debug.Log(currentState);
+       // Debug.Log(currentState);
         switch (currentState)
         {
             //move from one waypoint to another
             case State.Patrol:
-                agent.isStopped = false;
-                if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                    GoToNextPoint();
-
-                //if they spot the player, seek them
-                if (gameObject.GetComponent<Stealth>().detectedPlayer)
-                {
-                    currentSeekTime = 0;
-                    currentState = State.Seek;
-                }
-
-                //if they hear the player, go on alert
-                if (gameObject.GetComponent<Stealth>().heardPlayer)
-                {
-                    currentState = State.Alert;
-                }
+                Patrol();
                 break;
 
             //spin around for an amount of time
             case State.Alert:
-                //stop pathfinding
-                agent.isStopped = true;
-                agent.velocity = Vector3.zero;
-
-                //rotate around y axis
-                transform.Rotate(0, rotationPerSecond * currentDirection * Time.deltaTime, 0, 0);
-
-                //every 1/10th of a second, check if the agent should change direction
-                directionTimer += Time.deltaTime;
-                if (directionTimer >= 0.1f && (int)(Random.value * 100) <= percentChanceToChangeRotation)
-                {
-                    currentDirection *= -1;
-                }
-
-                if (directionTimer >= 0.1f)
-                {
-                    directionTimer = 0;
-                }
-
-                //after a bit of no sound, go back on patrol
-                if (!gameObject.GetComponent<Stealth>().heardPlayer)
-                {
-                    currentState = State.Patrol;
-                    currentWaypoint--;
-                    GoToNextPoint();
-                }
-
-                //if they see the player, seek them
-                if (gameObject.GetComponent<Stealth>().detectedPlayer)
-                {
-                    currentSeekTime = 0;
-                    currentState = State.Seek;
-                }
+                Alert();
                 break;
 
             //seek the player
             case State.Seek:
-                agent.isStopped = false;
-                agent.velocity = Vector3.zero;
-                GoToPlayerPoint();
-
-                //seek time increases
-                currentSeekTime += Time.deltaTime;
-
-                //if they see the player again or are within the seek radius, reset the time
-                if (gameObject.GetComponent<Stealth>().detectedPlayer ||
-                    gameObject.GetComponent<Stealth>().distanceToPlayer <= gameObject.GetComponent<Stealth>().alwaysPersueRadius)
-                {
-                    currentSeekTime = 0;
-                }
-
-                //if the agent catches the player, FOR NOW, go back to patrol
-                if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                {
-                    currentState = State.Patrol;
-                    GoToNextPoint();
-                }
-
-                //if the agent loses the player after a bit, go on alert
-                if (!gameObject.GetComponent<Stealth>().detectedPlayer && currentSeekTime >= seekTime)
-                {
-                    currentState = State.Alert;
-                }
+                
                 break;
         }
         
