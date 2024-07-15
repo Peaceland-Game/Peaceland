7795ed33302012f0d542c55a2cae6a92966afc9c
Owen Gahagan <114256905+PostVizsla@users.noreply.github.com>
2024-07-15 12:32:50 -0400

    Set up (non-final) artifact page layout and added artifact showing and hiding

--- Assets/Code/Scripts/UI/ArtifactJournalController.cs
+++ Assets/Code/Scripts/UI/ArtifactJournalController.cs
@@ -18,10 +18,11 @@ public class ArtifactJournalController : MonoBehaviour
         //get the artifact image components on the child objects
         artifacts = GetComponentsInChildren<ArtifactImage>().ToList();
     }
+
     /// <summary>
-    /// Attempts to reaveal the artifact by calling the child object's reveal function
+    /// Attempts to reveal the artifact by calling the child object's reveal function
     /// </summary>
-    /// <param name="name">the name of the artifact to reveal this should match the game object's name in Unity</param>
+    /// <param name="name">the name of the artifact to reveal. This should match the game object's name in Unity</param>
     /// <exception cref="System.Exception">Thrown when the name of the artifact does not match the scene object</exception>
     public void RevealArtifact(string name)
     {
@@ -34,4 +35,19 @@ public class ArtifactJournalController : MonoBehaviour
         artifact.RevealArtifact();
     }
 
+    /// <summary>
+    /// Attempts to hide the named artifact... in case you want to do that for some reason
+    /// </summary>
+    /// <param name="name">the name of the artifact to hide. This should match the game object's name in Unity</param>
+    /// <exception cref="System.Exception">Thrown when the name of the artifact does not match the scene object</exception>
+    public void HideArtifact(string name)
+    {
+        var artifact = artifacts.FirstOrDefault(artifact => artifact.gameObject.name == name);
+
+        if (!artifact)
+        {
+            throw new System.Exception($"Tried to hide missing artifact {name}");
+        }
+        artifact.HideArtifact();
+    }
 }
