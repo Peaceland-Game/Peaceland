36b9ce7911d160948c2e336e007893c59a19c310
Michael Eaton (RIT Student) <me3870@rit.edu>
2024-07-17 14:20:52 -0400

    sequencer stuff

---
+++ Assets/CameraTracker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void SetCameraRotation()
+    {
+
+    }
+}
