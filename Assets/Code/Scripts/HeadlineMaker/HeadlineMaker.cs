53baa2097d1894f9754f9975721d431f163fb86f
Michael Eaton (RIT Student) <me3870@rit.edu>
2024-07-18 12:52:57 -0400

    remove unused import for build reasons

--- Assets/Code/Scripts/HeadlineMaker/HeadlineMaker.cs
+++ Assets/Code/Scripts/HeadlineMaker/HeadlineMaker.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HeadlineMaker : MonoBehaviour
 {
